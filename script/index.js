bb90f2d3537e6bb208493abd62afa83c9dfb20c8
Trevor Mugo <trevormugolawrence@gmail.com>
2019-02-15 14:59:47 +0300

    logic2

--- script/index.js
+++ script/index.js
@@ -2,5 +2,33 @@
 var firstValue = document.getElementById("the-input1");
 var secondValue = document.getElementById("the-input2");
 var thirdValue = document.getElementById("the-input3");
-//check if all the input fields are there
-doc
+//check if all the input fields are there while button is not clicked
+
+function timer(){
+  setInterval(function(){
+    if(firstValue.value === "" || secondValue.value === "" || thirdValue.value === "")
+    {
+      //some or all the fields are empty
+      alert("fill in ll the required fields please");
+    }
+    else
+    {
+        //all the fiels are there
+    }
+  },30000);
+}
+//UNCOMMENT BELOW;
+//timer();
+var clickFunction = (()=>{
+  if(firstValue.value === "" || secondValue.value === "" || thirdValue.value === "")
+  {
+    //some or all the fields are empty
+    alert("fill in ll the required fields please because a triangle has 3 sides");
+  }
+  else
+  {
+      //all the fiels are there
+      console.log(firstValue.value + " " +secondValue.value+" "+ thirdValue.value);
+
+  }
+});
