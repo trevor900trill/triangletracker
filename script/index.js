a9845751e5784d201557bdcb4d845f36b3c01df3
Trevor Mugo <trevormugolawrence@gmail.com>
2019-02-15 13:41:14 +0300

    savepointcontent

--- script/index.js
+++ script/index.js
@@ -1,1 +1,4 @@
-console.log("hello world");
+//SYARY WEBSITE
+var firstValue = document.getElementById("the-input1");
+var secondValue = document.getElementById("the-input2");
+var thirdValue = document.getElementById("the-input3");
