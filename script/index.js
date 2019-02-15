781af4a20d28558d36472579f252c52c0c27aa91
Trevor Mugo <trevormugolawrence@gmail.com>
2019-02-15 13:47:56 +0300

    savepointcontentlogic

--- script/index.js
+++ script/index.js
@@ -2,3 +2,5 @@
 var firstValue = document.getElementById("the-input1");
 var secondValue = document.getElementById("the-input2");
 var thirdValue = document.getElementById("the-input3");
+//check if all the input fields are there
+doc
