433854979c9a573f86c63ba5396a87d71d65583d
Trevor Mugo <trevormugolawrence@gmail.com>
2019-02-16 16:38:40 +0300

    forw

--- script/index.js
+++ script/index.js
@@ -15,10 +15,12 @@ function timer(){
     {
         //all the fiels are there
     }
-  },30000);
+  },70000);
 }
 //UNCOMMENT BELOW;
 //timer();
+//ARRAY for history cache
+var cache = [];
 var clickFunction = (()=>{
   if(firstValue.value === "" || secondValue.value === "" || thirdValue.value === "")
   {
@@ -66,6 +68,9 @@ var clickFunction = (()=>{
               document.getElementsByClassName("show1")[0].textContent=ans1;
               document.getElementsByClassName("show2")[0].textContent=ans2;
               document.getElementsByClassName("show3")[0].textContent=ans3;
+              var text = "Equilateral triangle";
+              var smallsrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Triangle.Equilateral.svg/220px-Triangle.Equilateral.svg.png";
+              cache.push([smallsrc, text ,ans1,ans2,ans3]);
 
             }
             else if(ans1 === ans2 || ans2 === ans3 || ans1 === ans3)
@@ -77,6 +82,9 @@ var clickFunction = (()=>{
               document.getElementsByClassName("show1")[0].textContent=ans1;
               document.getElementsByClassName("show2")[0].textContent=ans2;
               document.getElementsByClassName("show3")[0].textContent=ans3;
+              var text2 = "isosceles triangle";
+              var smallsrc2 = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Triangle.Isosceles.svg/200px-Triangle.Isosceles.svg.png";
+              cache.push([smallsrc2,text2,ans1,ans2,ans3]);
             }
             else
             {
@@ -87,6 +95,9 @@ var clickFunction = (()=>{
               document.getElementsByClassName("show1")[0].textContent=ans1;
               document.getElementsByClassName("show2")[0].textContent=ans2;
               document.getElementsByClassName("show3")[0].textContent=ans3;
+              var text3 = "scalene triangle";
+              var smallsrc3 = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Triangle.Scalene.svg/245px-Triangle.Scalene.svg.png";
+              cache.push([smallsrc3,text3,ans1,ans2,ans3]);
             }
         }
       }
@@ -96,5 +107,51 @@ var clickFunction = (()=>{
         document.getElementsByClassName("triangleimage")[0].setAttribute("src","#");
         document.getElementsByClassName("descriptiontext")[0].textContent="Not a triangle";
       });
+  }
+});
+//Previous handle
+var count = 1;
+var previous = (() => {
+    //get click count
+    count = count + 1;
+    //get current position and go forward
+    var t = cache[cache.length - count];
+    if(t === undefined)
+    {
+      //do nothing if end has reached or max number of clicks;
+      alert("end of cache reload the page to start again");
+    }
+    else
+    {
+      console.log(t);
+      //render elements
+      document.getElementsByClassName("triangleimage")[0].setAttribute("src",t[0]);
+      document.getElementsByClassName("descriptiontext")[0].textContent=t[1];
+      document.getElementsByClassName("show1")[0].textContent=t[2];
+      document.getElementsByClassName("show2")[0].textContent=t[3];
+      document.getElementsByClassName("show3")[0].textContent=t[4];
+    }
+});
+//Forward handle
+var count2 = 0;
+var forward = (() => {
+  //get current position
+  var positionindex = cache.length - count;
+  //go Forward
+  count2 = count2 + 1;
+  var forward = cache[positionindex + count2];
+  if( forward === undefined)
+  {
+    //do nothing if end has reached or max number of clicks;
+    alert("end of cache reload the page to start again");
+  }
+  else
+  {
+    //render elements
+    document.getElementsByClassName("triangleimage")[0].setAttribute("src",forward[0]);
+    document.getElementsByClassName("descriptiontext")[0].textContent=forward[1];
+    document.getElementsByClassName("show1")[0].textContent=forward[2];
+    document.getElementsByClassName("show2")[0].textContent=forward[3];
+    document.getElementsByClassName("show3")[0].textContent=forward[4];
   }
 });
